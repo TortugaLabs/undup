c22ed0caf818f20f5930f0f2b16f576bea199491
Alejandro Liu <alejandro_liu@hotmail.com>
2015-11-18 09:53:57 +0100

    Refactor hashing code

--- calchash.c
+++ calchash.c
@@ -4,139 +4,121 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 
-#define MD5 '5'
-#define SHA256 '2'
 #ifndef HASH_TYPE
 #define HASH_TYPE MD5
 #endif
 
-#if HASH_TYPE == MD5
+//////////////////////////////////////////////////////////////////////
 //
 // Implements MD5
 //
+//////////////////////////////////////////////////////////////////////
 #include "lib/crypto-algorithms-master/md5.h"
 #include "lib/crypto-algorithms-master/md5.c"
-#define HLEN 16
+#define MD5_HLEN 16
 
-struct hash_ctx {
+struct md5_hash_ctx {
   MD5_CTX c;
-  char h[HLEN];
 };
+static char md5_name[] = "md5";
 
-int hash_type() {
-  return MD5;
-}
-int hash_len() {
-  return HLEN;
+static struct hash_ctx *md5_hash_new() {
+  struct md5_hash_ctx *ctx = mymalloc(sizeof(struct md5_hash_ctx));
+  memset(ctx,0,sizeof(struct md5_hash_ctx));
+  md5_init(&(ctx->c));
+  return (struct hash_ctx *)ctx;
 }
 
-void hash_init(struct hash_ctx *ctx) {
-  memset(ctx,0,sizeof(struct hash_ctx));
-  md5_init(&ctx->c);
-}
-struct hash_ctx *hash_new() {
-  struct hash_ctx *ctx = mymalloc(sizeof(struct hash_ctx));
-  hash_init(ctx);
-  return ctx;
-}
-void hash_update(struct hash_ctx *ctx,char *data,int len) {
+static void md5_hash_update(struct hash_ctx *ctxp,char *data,int len) {
+  struct md5_hash_ctx *ctx = (struct md5_hash_ctx *)ctxp;
   md5_update(&ctx->c,(BYTE *)data,len);
 }
-char *hash_get(struct hash_ctx *ctx) {
-  md5_final(&ctx->c,(BYTE *)ctx->h);
-  return ctx->h;
-}
-void hash_free(struct hash_ctx *ctx, char *hash) {
+static void md5_hash_free(struct hash_ctx *ctxp, char *hash) {
+  struct md5_hash_ctx *ctx = (struct md5_hash_ctx *)ctxp;
   if (hash) md5_final(&ctx->c,(BYTE *)hash);
   free(ctx);
 }
 
 
-#endif
-
-
-#if HASH_TYPE == SHA256
+//////////////////////////////////////////////////////////////////////
+//
+// Implements SHA256/SHA2
+//
+//////////////////////////////////////////////////////////////////////
 //
 // Implements SHA-256 (SHA-2)
 //
-#include <string.h>
+#undef ROTLEFT
 #include "lib/crypto-algorithms-master/sha256.h"
 #include "lib/crypto-algorithms-master/sha256.c"
-#define HLEN 32
+#define SHA256_HLEN 32
 
-struct hash_ctx {
+struct sha256_hash_ctx {
   SHA256_CTX c;
-  char h[HLEN];
 };
 
-int hash_type() {
-  return SHA256;
-}
-int hash_len() {
-  return HLEN;
-}
+static char sha256_name[] = "SHA256";
 
-void hash_init(struct hash_ctx *ctx) {
-  memset(ctx,0,sizeof(struct hash_ctx));
+static struct hash_ctx *sha256_hash_new() {
+  struct sha256_hash_ctx *ctx = mymalloc(sizeof(struct sha256_hash_ctx));
+  memset(ctx,0,sizeof(struct sha256_hash_ctx));
   sha256_init(&ctx->c);
+  return (struct hash_ctx *)ctx;
 }
-struct hash_ctx *hash_new() {
-  struct hash_ctx *ctx = mymalloc(sizeof(struct hash_ctx));
-  hash_init(ctx);
-  return ctx;
-}
-void hash_update(struct hash_ctx *ctx,char *data,int len) {
+static void sha256_hash_update(struct hash_ctx *ctxp,char *data,int len) {
+  struct sha256_hash_ctx *ctx = (struct sha256_hash_ctx *)ctxp;
   sha256_update(&ctx->c,(BYTE *)data,len);
 }
-char *hash_get(struct hash_ctx *ctx) {
-  sha256_final(&ctx->c,(BYTE *)ctx->h);
-  return ctx->h;
-}
-void hash_free(struct hash_ctx *ctx, char *hash) {
+static void sha256_hash_free(struct hash_ctx *ctxp, char *hash) {
+  struct sha256_hash_ctx *ctx = (struct sha256_hash_ctx *)ctxp;
   if (hash) sha256_final(&ctx->c,(BYTE *)hash);
   free(ctx);
 }
-#endif
+//////////////////////////////////////////////////////////////////////
 
-int _hash_open(const char *file,const char *root) {
-  int fd;
-  if (root) {
-    char *vfile = mystrcat(root,"/",file,NULL);
-    fd = open(vfile, O_RDONLY);
-    if (fd == -1) errorexit("open(%s)", file);
-    free(vfile);
-  } else {
-    fd = open(file, O_RDONLY);
-    if (fd == -1) errorexit("open(%s)", file);
+struct IHash calchash = {
+  .hash_type = MD5,
+  .hash_len = MD5_HLEN,
+  .hash_new_fn = md5_hash_new,
+  .hash_update_fn = md5_hash_update,
+  .hash_free_fn = md5_hash_free,
+  .hash_name = md5_name,
+};
+
+void hash_set(int type) {
+  switch (type) {
+  case MD5:
+    calchash.hash_type = MD5;
+    calchash.hash_len = MD5_HLEN;
+    calchash.hash_new_fn = md5_hash_new;
+    calchash.hash_update_fn = md5_hash_update;
+    calchash.hash_free_fn = md5_hash_free;
+    calchash.hash_name = md5_name;
+    break;
+  case SHA256:
+    calchash.hash_type = SHA256;
+    calchash.hash_len = SHA256_HLEN;
+    calchash.hash_new_fn = sha256_hash_new;
+    calchash.hash_update_fn = sha256_hash_update;
+    calchash.hash_free_fn = sha256_hash_free;
+    calchash.hash_name = sha256_name;
+    break;
+  default:
+    fatal(EINVAL,"Invalid hash type %x\n", type);
   }
-  return fd;
 }
-char *hash_part(const char *file,const char *root) {
+
+//////////////////////////////////////////////////////////////////////
+char *hash_file(const char *file) {
   int fd, cnt;
   struct hash_ctx *ctx;
   char buf[_HASH_BUFSZ], *res;
 
-  fd = _hash_open(file, root);
-  ctx = hash_new();
-  cnt = read(fd,buf,sizeof buf);
-  switch (cnt) {
-  case -1:
-    errorexit("read(%s)",file);
-  case 0:
-    fatal(ENODATA,"empty read(%s)",file);
-  }
-  hash_update(ctx, buf, cnt);
-  res = mymalloc(hash_len());
-  hash_free(ctx,res);
-  return res;
-}
-char *hash_full(const char *file,const char *root) {
-  int fd, cnt;
-  struct hash_ctx *ctx;
-  char buf[_HASH_BUFSZ], *res;
+  fd = open(file, O_RDONLY);
+  if (fd == -1) errorexit("open(%s)", file);
 
-  fd = _hash_open(file,root);
   ctx = hash_new();
   while ((cnt = read(fd,buf,sizeof buf)) == 0) {
     if (cnt == -1) errorexit("read(%s)",file);
--- calchash.h
+++ calchash.h
@@ -3,14 +3,26 @@
 #define _HASH_BUFSZ 4096
 struct hash_ctx;
 
-int hash_type();
-int hash_len();
-struct hash_ctx *hash_new();
-void hash_init(struct hash_ctx *ctx);
-void hash_update(struct hash_ctx *ctx,char *data,int len);
-char *hash_get(struct hash_ctx *ctx);
-void hash_free(struct hash_ctx *ctx, char *hash);
+#define MD5 '5'
+#define SHA256 '2'
 
-char *hash_part(const char *file,const char *root);
-char *hash_full(const char *file,const char *root);
+struct IHash {
+  int hash_type;
+  int hash_len;
+  struct hash_ctx *(*hash_new_fn)();
+  void (*hash_update_fn)(struct hash_ctx *ctx,char *data,int len);
+  void (*hash_free_fn)(struct hash_ctx *ctx,char *hash);
+  const char *hash_name;
+};
+extern struct IHash calchash;
+
+void hash_set(int type);
+
+#define hash_type() (calchash.hash_type)
+#define hash_len() (calchash.hash_len)
+#define hash_name() (calchash.hash_name)
+#define hash_new() calchash.hash_new_fn()
+#define hash_update(ctx,dat,len) calchash.hash_update_fn(ctx,dat,len)
+#define hash_free(ctx,h) calchash.hash_free_fn(ctx,h)
+char *hash_file(const char *file);
 #endif
