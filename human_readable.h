5d334eb56211f5e00f8ef0b2fc4228997cd1fbdb
Alejandro Liu <alejandro_liu@hotmail.com>
2015-11-24 14:48:03 +0100

    Added human_readable from busybox libbb

---
+++ human_readable.h
@@ -0,0 +1,6 @@
+#ifndef _HUMAN_READABLE_H
+#define _HUMAN_READABLE_H
+const char* make_human_readable_str(unsigned long long val,
+				    unsigned long block_size,
+				    unsigned long display_unit);
+#endif
